ac2a3b5d271c8d7b68eb0463063f5caa742983da
Terrence Gaitan <terrencebohorquezgaitan@gmail.com>
2020-09-27 20:13:56 -0500

    pendiente informaciones y panel de testimonios

---
+++ liftit.js
@@ -0,0 +1,11 @@
+var rellax = new Rellax(".rellax", {
+    horizontal: true,
+    vertical: true,
+    speed: -0.7
+});
+var Movi = new Movi(".Movi", {
+    horizontal: false,
+    vertical: true, 
+    speed: 0.9,
+    
+});
